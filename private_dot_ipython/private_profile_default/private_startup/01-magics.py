b84b155be89c04c5d6eeb33a7be78951386de8c9
Kevin Wu <kevin@kevinniuwu.com>
2024-02-07 21:24:40 -0500

    Fix clip magic on macOS

--- private_dot_ipython/private_profile_default/private_startup/01-magics.py
+++ private_dot_ipython/private_profile_default/private_startup/01-magics.py
@@ -1,31 +1,42 @@
 "Adapted from https://gist.github.com/nova77/5403446"
-
 import sys
+from subprocess import Popen, PIPE
 from contextlib import redirect_stdout
 import io
 
-if sys.platform == 'darwin':
-    from AppKit import NSPasteboard, NSArray
-elif sys.platform.startswith('linux'):
-    from subprocess import Popen, PIPE
-else:
-    raise ImportError("Clip magic only works on osx or linux!")
-
 from IPython.core.magic import register_line_cell_magic, register_line_magic
+
+def _get_implementation():
+    try:
+        _get_implementation.impls
+    except AttributeError:
+        _get_implementation.impls = {}
+
+        if sys.platform.startswith('linux'):
+            def _clip(arg):
+                p = Popen(['xclip', '-selection', 'clipboard'], stdin=PIPE)
+                p.communicate(arg)
+            _get_implementation.impls['linux'] = _clip
+
+        elif sys.platform == 'darwin':
+            def _clip(arg):
+                p = Popen('pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=PIPE)
+                p.communicate(arg.encode('utf-8'))
+            _get_implementation.impls[sys.platform] = _clip
+
+        # add further platforms below here:
+        else:
+            raise ImportError("Clip magic doesn't work on your platform: '{}'".format(sys.platform))
+
+
+    return _get_implementation['linux'] \
+            if sys.platform.startswith('linux') \
+        else _get_implementation.impls[sys.platform]
 
 
 def _copy_to_clipboard(arg):
     arg = str(globals().get(arg) or arg)
-
-    if sys.platform == 'darwin':
-        pb = NSPasteboard.generalPasteboard()
-        pb.clearContents()
-        a = NSArray.arrayWithObject_(arg)
-        pb.writeObjects_(a)
-    elif sys.platform.startswith('linux'):
-        p = Popen(['xclip', '-selection', 'clipboard'], stdin=PIPE)
-        p.communicate(input=arg.encode('utf-8'))
-
+    _get_implementation()(arg)
     print('Copied to clipboard!')
 
 
